2ee44495ac250b113abdbe63397a2f14faef7732
Sahil <sohil170246@gmail.com>
2023-11-21 23:51:40 +0530

    basic ui setup api intergration and functionality remaining

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header/header"
+import Header from "./components/header/Header"
 import Hero from "./components/hero/Hero"
 import Main from "./components/main/Main"
 function App() {

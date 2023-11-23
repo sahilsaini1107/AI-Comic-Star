8656cda022bf9b6026bf0bd86f25e901a9951ca6
Sahil <sohil170246@gmail.com>
2023-11-23 15:57:42 +0530

    made compatible for all screen sizes

--- src/components/herocanvas/HeroCanvas.jsx
+++ src/components/herocanvas/HeroCanvas.jsx
@@ -1,5 +1,4 @@
-import React, { useRef, useEffect} from 'react';
-import Styles from './HeroCanvas.module.css';
+import React, { useRef, useEffect } from 'react';
 import heroImage from '../../assets/4833725.jpg';
 
 const HeroCanvas = ({ images }) => {
@@ -24,60 +23,42 @@ const HeroCanvas = ({ images }) => {
       canvas.width = width;
       canvas.height = height;
 
-      const gap = 10; // Adjust the gap between images
-      const imageWidth = 300; // Adjust the image width
+      const gapX = 15; // gap between columns
+      const gapY = 15; // gap between rows
+
+      // const cellWidth = width / 4;
+      // const cellHeight = height / 3;
+      const cellWidth = ((width - (gapX * 3)) / 4) -30; // Adjusting for gaps
+      const cellHeight = ((height - (gapY * 2)) / 3)-15; // Adjusting for gaps
+    
 
       const image = new Image();
       image.onload = () => {
         ctx.drawImage(image, 0, 0, width, height);
-
-        // Calculate total images width including gaps
-        const totalImagesWidth = (images.length - 1) * gap + images.length * imageWidth;
-
-        // Starting X position to center the images horizontally
-        let startX = (width - totalImagesWidth) / 2;
-        const startY = height * 0.1; // Set a fixed starting Y position
-
         images.forEach((url, index) => {
+          const row = Math.floor(index / 4);
+          const col = index % 4;
+
           const img = new Image();
           img.onload = () => {
-            const rect = image.getBoundingClientRect();
-            let imageX;
-            let imageY;
-            if(index==0){
-              imageX = 50;
-              imageY = 30;
-            }else if(index<4){
-              imageX = (100) * (index*3.5);
-              imageY = 30;
-            }else if(index === 4){ 
-              imageX = 50;
-              imageY = 360;
-            }else if(index<8){
-              let x = index-4;
-              imageX = 100 * (x*3.5);
-              imageY = 360;
-            }else if(index === 8){
-              imageX = 50;
-              imageY = 690;
-            }else{
-              let x = index-8;
-              imageX = 100 * (x*3.5);
-              imageY = 360;
-            }
+            // const imageX = col * cellWidth + 40;
+            // const imageY = row * cellHeight + 20 ;
+            const imageX = col * (cellWidth + gapX) + 60; 
+            const imageY = row * (cellHeight + gapY) + 20;
 
-            ctx.filter = 'grayscale(100%)';
-            ctx.drawImage(img, imageX, imageY, imageWidth, 300); // Scale height based on canvas height
-            ctx.strokeStyle = 'black';
-            ctx.lineWidth = 4;
-            ctx.strokeRect(imageX, imageY, imageWidth, 300);
+            // ctx.filter = 'grayscale(100%)';
+            ctx.drawImage(img, imageX, imageY, cellWidth, cellHeight);
+            ctx.strokeStyle = 'aqua';
+            ctx.lineWidth = 8;
+            ctx.strokeRect(imageX, imageY, cellWidth, cellHeight);
 
             // Other drawing operations (circles, text, etc.)
             ctx.font = '16px Arial';
-            const textWidth = ctx.measureText(url.anno).width;
-            const fontSize = Math.min(16, 200 / (textWidth || 1));
+            // Replace url.anno with a string or the appropriate property from the images object
+            const text = 'Sample Text'; // Replace with the desired text
+            const textWidth = ctx.measureText(text).width;
+            const fontSize = Math.min(16, Math.min(cellWidth, cellHeight) / (textWidth || 1));
 
-            const circle1Radius = (textWidth > 20) ? (textWidth + 10) / 2 : 20;
             const circle1X = imageX + 27;
             const circle1Y = imageY + 27;
             ctx.beginPath();
@@ -102,9 +83,7 @@ const HeroCanvas = ({ images }) => {
             ctx.textAlign = 'center';
             ctx.font = `${fontSize}px Arial`;
             ctx.textBaseline = 'middle';
-            ctx.fillText(url.anno, circle1X, circle1Y);
-            // Update start X position for the next image
-            startX += imageWidth + gap;
+            ctx.fillText(text, circle1X, circle1Y); // Replace url.anno with the desired text variable
           };
           img.onerror = (error) => {
             console.error('Error loading image:', error);
@@ -123,130 +102,29 @@ const HeroCanvas = ({ images }) => {
     };
   }, [images]);
 
-  // Rest of the component remains the same
+  const downloadCanvas = () => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      console.error('Canvas element not found');
+      return;
+    }
+    const link = document.createElement('a');
+    link.download = 'comic_page.png';
+    link.href = canvas.toDataURL('image/png');
+    link.click();
+  };
 
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       <canvas ref={canvasRef} />
-      {/* ... */}
+      <button
+        className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+        onClick={downloadCanvas}
+      >
+        Download Comic
+      </button>
     </div>
   );
 };
 
 export default HeroCanvas;
-
-
-
-// const HeroCanvas = ({ images }) => {
-//   const canvasRef = useRef(null);
-//   useEffect(() => {
-//     const canvas = canvasRef.current;
-//     if (!canvas) {
-//       console.error('Canvas element not found');
-//       return;
-//     }
-//     const ctx = canvas.getContext('2d');
-//     if (!ctx) {
-//       console.error('Canvas context not supported');
-//       return;
-//     }
-
-//     canvas.width = window.innerWidth; // Set canvas width based on window width
-//     canvas.height = window.innerHeight;
-
-//     images.forEach((url, index) => {
-//       const img = new Image();
-//       img.onload = () => {
-//         let imageX;
-//         let imageY;
-//         if(index==0){
-//           imageX = 20;
-//           imageY = 30;
-//         }else if(index<4){
-//           imageX = (100) * (index*3.5);
-//           imageY = 30;
-//         }else if(index === 4){ 
-//           imageX = 22;
-//           imageY = 360;
-//         }else if(index<8){
-//           let x = index-4;
-//           imageX = 100 * (x*3.5);
-//           imageY = 360;
-//         }else if(index === 8){
-//           imageX = 22;
-//           imageY = 690;
-//         }else{
-//           let x = index-8;
-//           imageX = 100 * (x*3.5);
-//           imageY = 360;
-//         }
-        
-//         ctx.filter = 'grayscale(100%)';
-//         ctx.drawImage(img, imageX, imageY, 300, 300);
-//         ctx.strokeStyle = 'black';
-//         ctx.lineWidth = 4;
-//         ctx.strokeRect(imageX, imageY, 300, 300);
-
-//         ctx.font = '16px Arial';
-//         const textWidth = ctx.measureText(url.anno).width;
-//         const fontSize = Math.min(16, 200 / (textWidth || 1));
-
-//         const circle1Radius = (textWidth > 20) ? (textWidth + 10) / 2 : 20;
-//         const circle1X = imageX + 27;
-//         const circle1Y = imageY + 27;
-//         ctx.beginPath();
-//         ctx.arc(circle1X, circle1Y, 20, 0, 2 * Math.PI);
-//         ctx.fillStyle = 'white';
-//         ctx.fill();
-//         ctx.strokeStyle = 'aqua';
-//         ctx.lineWidth = 2;
-//         ctx.stroke();
-
-//         const circle2X = imageX + 55;
-//         const circle2Y = imageY + 45;
-//         ctx.beginPath();
-//         ctx.arc(circle2X, circle2Y, 8, 0, 2 * Math.PI);
-//         ctx.fillStyle = 'white';
-//         ctx.fill();
-//         ctx.strokeStyle = 'aqua';
-//         ctx.lineWidth = 2;
-//         ctx.stroke();
-
-//         ctx.fillStyle = 'black';
-//         ctx.textAlign = 'center';
-//         ctx.font = `${fontSize}px Arial`;
-//         ctx.textBaseline = 'middle';
-//         ctx.fillText(url.anno, circle1X, circle1Y);
-//       };
-//       img.onerror = (error) => {
-//         console.error('Error loading image:', error);
-//       };
-//       img.src = url.url;
-//     });
-//   }, [images]);
-
-//   const downloadCanvas = () => {
-//     const canvas = canvasRef.current;
-//     if (!canvas) {
-//       console.error('Canvas element not found');
-//       return;
-//     }
-//     const link = document.createElement('a');
-//     link.download = 'comic_page.png';
-//     link.href = canvas.toDataURL('image/png');
-//     link.click();
-//   };
-
-//   return (
-//     <div>
-//       <canvas ref={canvasRef} />
-//       <center>
-//         <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' onClick={downloadCanvas}>
-//           Download Comic
-//         </button>
-//       </center>
-//     </div>
-//   );
-// };
-
-// export default HeroCanvas;
